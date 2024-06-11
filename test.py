30459374f2b78f0b8ef629f521c08712d3fe18b3
francescoboc <francescoboc@gmail.com>
2024-06-11 15:10:47 +0200

    script to test snake game

---
+++ test.py
@@ -0,0 +1,11 @@
+from tools import *
+from snake import *
+
+# snake parameters
+cell_size = 25
+box_size = 30
+snake_speed = 15
+periodic = True
+
+game = Snake(cell_size, box_size, snake_speed, periodic)
+game.play()
