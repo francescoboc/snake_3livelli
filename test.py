93e69d8524e58cc1909177447fc49103cbcddbdc
Luca Gagliardi <lucaj.1991@gmail.com>
2024-06-12 17:54:39 +0200

    minor

--- test.py
+++ test.py
@@ -6,11 +6,13 @@ from importlib import reload
 reload(sys.modules['snake'])
 from snake import *
 
+actionMode = 4
+
 # snake parameters
 cell_size = 25
 box_size = 30
-snake_speed = 15
+snake_speed = 10
 periodic = True
 
-game = Snake(cell_size, box_size, snake_speed, periodic)
+game = Snake(actionMode)
 game.play()
