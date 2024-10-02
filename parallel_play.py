e568492ab38dbb3a0138de2c3efc28b39de3b45f
francescoboc <francescoboc@gmail.com>
2024-10-02 18:23:15 +0200

    remove global variables and use shared_vars list

--- parallel_play.py
+++ parallel_play.py
@@ -9,11 +9,74 @@ from snake import *
 
 # TODO add a countdown in snake!
 
-# function to set global variables
-def set_global_variables():
-    global box_size, snake_speed, periodic, action_mode, rand_init_body_length,\
-        rand_init_direction, state_mode, show_compass, sound_effects, show_state_info
+# function to run each snake game in a separate process
+def run_snake_game(policy, game_id, window_position, cell_size, shared_vars):
+    # unpack shared variables
+    box_size, snake_speed, periodic, action_mode, rand_init_body_length,\
+        rand_init_direction, state_mode, show_compass, sound_effects, show_state_info = shared_vars 
 
+    # seed to initialize the RNG
+    seed = 666
+    seed_rng(seed, verbose=False)
+
+    # create snake game object
+    snake = Snake(action_mode, state_mode, cell_size, box_size, snake_speed, periodic, 
+            rand_init_body_length, rand_init_direction, show_compass, sound_effects, 
+            show_state_info, window_position, 'test team', verbose=False)
+
+    # Play the game with the provided policy
+    snake.play(policy)
+
+# function to launch multiple games in parallel
+def run_games_in_parallel(policies, shared_vars):
+    # unpack shared variables
+    box_size, snake_speed, periodic, action_mode, rand_init_body_length,\
+        rand_init_direction, state_mode, show_compass, sound_effects, show_state_info = shared_vars 
+
+    # count policies to get number of teams
+    n_teams = len(policies)
+
+    if n_teams > 6: raise Exception('The maximum number of supported policies is 6')
+
+    # get screen resolution
+    pygame.display.init()
+    display_sizes = pygame.display.get_desktop_sizes()
+    pygame.display.quit()
+    if len(display_sizes)>1:
+        resolution = display_sizes[1]
+        print(f'Second screen detected with resolution {resolution}')
+    else:
+        resolution = display_sizes[0]
+        print(f'One screen detected with resolution {resolution}')
+    screen_width, screen_heigth = resolution[0], resolution[1]
+
+    margin = 0
+
+    # window parameters
+    if n_teams == 6:
+        cell_size = min( (screen_width//3)//box_size, (screen_heigth//2)//box_size )
+        window_width = cell_size*box_size
+        window_positions = []
+        for l in range(2):
+            for c in range(3):
+                shift_x = (window_width + margin)*c
+                # eventual fine tuning of position
+                # shift_x += (window_width + margin)//3
+                window_positions.append((shift_x, window_width*l))
+
+    # create a new process for each game
+    processes = []
+    for i, policy in enumerate(policies):
+        p = multiprocessing.Process(target=run_snake_game, args=(policy, i+1,
+            window_positions[i], cell_size, shared_vars))
+        processes.append(p)
+        p.start()
+
+    # wait for all processes to finish
+    for p in processes:
+        p.join()
+
+if __name__ == "__main__":
     # snake parameters
     box_size = 30
     snake_speed = 10
@@ -33,66 +96,9 @@ def set_global_variables():
     sound_effects = False
     show_state_info = False
 
-# function to run each snake game in a separate process
-def run_snake_game(policy, game_id, window_position):
-    # seed to initialize the RNG
-    seed = 666
-    seed_rng(seed, verbose=False)
-
-    # create snake game object
-    snake = Snake(action_mode, state_mode, cell_size, box_size, snake_speed, periodic, 
-            rand_init_body_length, rand_init_direction, show_compass, sound_effects, 
-            show_state_info, window_position, 'test team', verbose=False)
-
-    # Play the game with the provided policy
-    snake.play(policy)
-
-# function to launch multiple games in parallel
-def run_games_in_parallel(policies):
-    # count policies to get number of teams
-    n_teams = len(policies)
-
-    if n_teams > 6: raise Exception('The maximum number of supported policies is 6')
-
-    # get screen resolution
-    pygame.display.init()
-    display_sizes = pygame.display.get_desktop_sizes()
-    pygame.display.quit()
-    if len(display_sizes)>1:
-        resolution = display_sizes[1]
-        print(f'Secondary screen detected with resolution {resolution}')
-    else:
-        resolution = display_sizes[0]
-
-    margin = 0
-
-    global cell_size
-
-    # window parameters
-    if n_teams == 6:
-        cell_size = (resolution[0]//3)//box_size
-        window_width = cell_size*box_size
-        window_positions = []
-        for l in range(2):
-            for c in range(3):
-                shift_x = (window_width + margin)*c
-                # eventual fine tuning of position
-                # shift_x += (window_width + margin)//3
-                window_positions.append((shift_x, window_width*l))
-
-    # create a new process for each game
-    processes = []
-    for i, policy in enumerate(policies):
-        p = multiprocessing.Process(target=run_snake_game, args=(policy, i+1, window_positions[i]))
-        processes.append(p)
-        p.start()
-
-    # wait for all processes to finish
-    for p in processes:
-        p.join()
-
-if __name__ == "__main__":
-    set_global_variables()
+    # put all shared variables into a list for convenience
+    shared_vars = [box_size, snake_speed, periodic, action_mode, rand_init_body_length,\
+        rand_init_direction, state_mode, show_compass, sound_effects, show_state_info]
 
     # load different policies (pi1, pi2, pi3...)
     n_episodes = int(1e7)
@@ -104,4 +110,4 @@ if __name__ == "__main__":
     policies = [pi1, pi1, pi1, pi1, pi1, pi1]  
 
     # run the games in parallel
-    run_games_in_parallel(policies)
+    run_games_in_parallel(policies, shared_vars)
